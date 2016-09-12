7db2a35da75916a63c98578ec5254995dd25cd25
forrest <forrest0402@gmail.com>
2016-09-12 16:10:15 +0800

    compelte my first submit in leetcode

---
+++ C#/344. Reverse String.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public string ReverseString(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        int lPos = 0, rPos = s.Length - 1;
+        while (lPos < rPos)
+        {
+            char tempC = charArray[lPos];
+            charArray[lPos] = charArray[rPos];
+            charArray[rPos] = tempC;
+            ++lPos;
+            --rPos;
+        }
+        return new string(charArray);
+    }
+}
