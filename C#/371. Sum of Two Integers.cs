57de7d7f5b7fc6a172a32ead773c5a65b06aafe4
forrest <forrest0402@gmail.com>
2016-09-13 11:25:07 +0800

    3

---
+++ C#/371. Sum of Two Integers.cs
@@ -0,0 +1,44 @@
+﻿// basic idea - summator
+public class Solution
+{
+    public int GetSum(int a, int b)
+    {
+        string n1 = Convert.ToString(a, 2).PadLeft(32, '0');
+        string n2 = Convert.ToString(b, 2).PadLeft(32, '0');
+        string ans = "";
+        int flag = 0;
+        for (int i = 31; i >= 0; --i)
+        {
+            ans = (((int)n1[i]) ^ ((int)n2[i]) ^ flag) + ans;
+            if ((n1[i] == '1' && n2[i] == '1') || (n1[i] == '1' && flag == 1) || (flag == 1 && n2[i] == '1'))
+                flag = 1;
+            else flag = 0;
+        }
+        return Convert.ToInt32(ans, 2);
+    }
+}
+
+// bit operation - version 1
+public class Solution
+{
+    public int GetSum(int a, int b)
+    {
+        int sum = 0;
+        while (b != 0)
+        {
+            sum = a ^ b;
+            b = (a & b) << 1;
+            a = sum;
+        }
+        return sum;
+    }
+}
+
+// bit operation - version 2
+public class Solution
+{
+    public int GetSum(int a, int b)
+    {
+        return b == 0 ? a : GetSum(a ^ b, (a & b) << 1);
+    }
+}
--- README.md
+++ README.md
@@ -5,5 +5,11 @@
 
 [Reverse String] - 20160912
 
+## 371. Sum of Two Integers
+>We can use xor to simulate addition. And use add and shifting to position carry.
+
+[Sum of Two Integers] - 20160913
+
 [LeetCode OJ]:https://leetcode.com/
-[Reverse String]: https://leetcode.com/problems/reverse-string/ 
+[Reverse String]: https://leetcode.com/problems/reverse-string/
+[Sum of Two Integers]:https://leetcode.com/problems/sum-of-two-integers/
