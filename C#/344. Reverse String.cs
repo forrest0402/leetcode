8efa6b18a44bed3ced3c50376a37273368d3b62a
forrest <forrest0402@gmail.com>
2016-09-13 19:50:50 +0800

    my first submission in leetcode

--- C#/344. Reverse String.cs
+++ C#/344. Reverse String.cs
@@ -9,7 +9,6 @@
             char tempC = charArray[lPos];
             charArray[lPos] = charArray[rPos];
             charArray[rPos] = tempC;
-            charArray.Length
         }
         return new string(charArray);
     }
