fcc00ed10126ce8ec4a079b6b68a5c5425135994
forrest <forrest0402@gmail.com>
2016-09-13 20:13:44 +0800

    11-2

---
+++ C#/383. Ransom Note.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        bool[] used = new bool[magazine.Length];
+        for (int i = 0; i < ransomNote.Length; ++i)
+        {
+            bool flag = false;
+            for (int j = 0; j < magazine.Length; ++j)
+            {
+                if (!used[j] && magazine[j] == ransomNote[i])
+                {
+                    flag = true;
+                    used[j] = true;
+                    break;
+                }
+            }
+            if (!flag)
+                return false;
+        }
+        return true;
+    }
+}
