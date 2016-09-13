d541b8963cb63fecf38b1331deda906c91568dac
forrest <forrest0402@gmail.com>
2016-09-13 20:50:32 +0800

    13

---
+++ C#/349. Intersection of Two Arrays.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        HashSet<int> set1 = new HashSet<int>();
+        HashSet<int> set2 = new HashSet<int>();
+        foreach (int i in nums1)
+            set1.Add(i);
+        foreach (int i in nums2)
+            if (set1.Contains(i))
+                set2.Add(i);
+        return set2.ToArray();
+    }
+}
