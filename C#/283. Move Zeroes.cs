32ab05ed3244be31109b5f5bc6e1a7d778aee8a0
forrest <forrest0402@gmail.com>
2016-09-13 19:06:19 +0800

    9

---
+++ C#/283. Move Zeroes.cs
@@ -0,0 +1,12 @@
+﻿public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int start = 0;
+        for (int i = 0; i < nums.Length; ++i)
+            if (nums[i] != 0)
+                nums[start++] = nums[i];
+        for (int i = start; i < nums.Length; ++i)
+            nums[i] = 0;
+    }
+}
