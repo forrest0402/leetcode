c13b8beec81c99f54cd64f712061dee6c9da4c12
forrest <forrest0402@gmail.com>
2016-09-13 11:40:50 +0800

    4

---
+++ C#/136. Single Number.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int ans = 0;
+        foreach (int i in nums)
+            ans ^= i;
+        return ans;
+    }
+}
