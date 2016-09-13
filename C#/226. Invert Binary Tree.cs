f074e09d35a38f9dc4e3fe4dd30e77970ff4f12b
forrest <forrest0402@gmail.com>
2016-09-13 18:54:39 +0800

    8

---
+++ C#/226. Invert Binary Tree.cs
@@ -0,0 +1,23 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public TreeNode InvertTree(TreeNode root)
+    {
+        if (root == null)
+            return null;
+        TreeNode tempNode = root.left;
+        root.left = root.right;
+        root.right = tempNode;
+        root.left = InvertTree(root.left);
+        root.right = InvertTree(root.right);
+        return root;
+    }
+}
