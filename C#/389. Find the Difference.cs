e1f006b2553548bc14ea412095dd6a3363189652
forrest <forrest0402@gmail.com>
2016-09-13 14:55:44 +0800

    7

---
+++ C#/389. Find the Difference.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+        int[] array = new int[123];
+        foreach (char ch in s)
+            array[(int)ch]++;
+        foreach (char ch in t)
+            array[(int)ch]--;
+        for (int i = 0; i < 123; ++i)
+            if (array[i] < 0)
+                return (char)i;
+        return 'a';
+    }
+}
