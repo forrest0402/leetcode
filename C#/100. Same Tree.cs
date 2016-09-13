9dc7a06dfe456e49171fc44d7e953fa0d49f336c
forrest <forrest0402@gmail.com>
2016-09-13 20:41:06 +0800

    12

---
+++ C#/100. Same Tree.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public bool IsSameTree(TreeNode p, TreeNode q)
+    {
+        if (p == null && q == null)
+            return true;
+        else if (p != null && q != null)
+        {
+            if (p.val != q.val)
+                return false;
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+        return false;
+    }
+}
