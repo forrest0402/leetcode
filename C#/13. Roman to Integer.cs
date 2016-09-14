5816d8ff4ff17047d06acbd9d212b4a306bc9f59
forrest <forrest0402@gmail.com>
2016-09-14 22:23:32 +0800

    23

---
+++ C#/13. Roman to Integer.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    Dictionary<char, int> dic = new Dictionary<char, int>{
+        {'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000}
+    };
+    public int RomanToInt(string s)
+    {
+        int count = 0;
+        char pre = 'M';
+        foreach (char chs in s)
+        {
+            count += dic[chs];
+            if (dic[chs] > dic[pre])
+            {
+                count -= 2 * dic[pre];
+            }
+            pre = chs;
+        }
+        return count;
+    }
+}
