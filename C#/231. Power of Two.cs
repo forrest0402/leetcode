c549e7bbbdcddd0055b73b961bd80dcc4c62d0a3
forrest <forrest0402@gmail.com>
2016-09-14 10:23:11 +0800

    14-3

---
+++ C#/231. Power of Two.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n == 0)
+            return false;
+        double ans = Math.Log(n) / Math.Log(2);
+        return ans - (int)ans < 1e-10;
+    }
+}
