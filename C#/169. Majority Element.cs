da3aa3e7dc6c93437dbf0928906131b43eee9a54
forrest <forrest0402@gmail.com>
2016-09-14 14:57:00 +0800

    20

---
+++ C#/169. Majority Element.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        foreach (int i in nums)
+        {
+            if (dic.ContainsKey(i))
+                dic[i]++;
+            else dic.Add(i, 1);
+        }
+        int num = 0, element = 0;
+        foreach (int key in dic.Keys)
+        {
+            if (dic[key] > num)
+            {
+                num = dic[key];
+                element = key;
+            }
+        }
+        return element;
+    }
+}
