3e1595ed311901b725244af1f31fad3a65f53103
forrest <forrest0402@gmail.com>
2016-09-14 15:28:13 +0800

    21

---
+++ C#/242. Valid Anagram.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        char[] chars = s.ToCharArray();
+        char[] chart = t.ToCharArray();
+        Array.Sort(chars);
+        Array.Sort(chart);
+        return new string(chars) == new string(chart);
+    }
+}
