70d94d46e632c3993af7066b511d22caeca04f46
forrest <forrest0402@gmail.com>
2016-09-15 14:31:23 +0800

    29

---
+++ C#/342. Power of Four.cs
@@ -0,0 +1,8 @@
+﻿public class Solution
+{
+    public bool IsPowerOfFour(int num)
+    {
+        double ans = Math.Log(num, 4);
+        return Math.Min(ans - Math.Floor(ans), Math.Ceiling(ans) - ans) < 1e-10;
+    }
+}
