89d6f9d8319e0d07c3a2c4c3fe5ce270dfc3784f
forrest <forrest0402@gmail.com>
2016-09-15 14:27:08 +0800

    28

---
+++ C#/70. Climbing Stairs.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public double Factorial(int n)
+    {
+        double ans = 1;
+        for (int i = 2; i <= n; ++i)
+            ans *= i;
+        return ans;
+    }
+    public int ClimbStairs(int n)
+    {
+        double count = 0;
+        int step1 = n, step2 = 0;
+        while (step1 >= 0)
+        {
+            count += Factorial(step1 + step2) / (Factorial(step1) * Factorial(step2));
+            step1 -= 2;
+            ++step2;
+        }
+        return (int)count;
+    }
+}
