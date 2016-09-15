4df3ba2eafaf0056b22d831530363b034449ff35
forrest <forrest0402@gmail.com>
2016-09-15 15:14:09 +0800

    32

---
+++ C#/121. Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int maxProfit = 0, minBuy = int.MaxValue;
+        for (int i = 0; i < prices.Length; ++i)
+        {
+            if (prices[i] - minBuy > maxProfit)
+                maxProfit = prices[i] - minBuy;
+            if (prices[i] < minBuy)
+                minBuy = prices[i];
+        }
+        return maxProfit;
+    }
+}
