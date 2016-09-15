7dffdb7a9be7de54f9b4e1b3edf89d3c339797d7
forrest <forrest0402@gmail.com>
2016-09-15 11:47:28 +0800

    26

---
+++ C#/191. Number of 1 Bits.cs
@@ -0,0 +1,23 @@
+﻿//trick
+public class Solution
+{
+    public int HammingWeight(uint n)
+    {
+        return Convert.ToString(n, 2).Replace("0", "").Length;
+    }
+}
+
+//
+public class Solution
+{
+    public int HammingWeight(uint n)
+    {
+        int count = 0;
+        while (n > 0)
+        {
+            count += n % 2 == 0 ? 0 : 1;
+            n /= 2;
+        }
+        return count;
+    }
+}
