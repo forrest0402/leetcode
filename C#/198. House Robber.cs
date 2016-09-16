7228ec3effb9d7f8c49d4d6f76a63f6daf378497
forrest <forrest0402@gmail.com>
2016-09-16 13:13:51 +0800

    35-2

---
+++ C#/198. House Robber.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        if (nums.Length == 0)
+            return 0;
+        if (nums.Length == 1)
+            return nums[0];
+        if (nums.Length == 2)
+            return nums[0] > nums[1] ? nums[0] : nums[1];
+        int[] array = new int[nums.Length];
+        array[0] = nums[0];
+        array[1] = nums[1] > nums[0] ? nums[1] : nums[0];
+        for (int i = 2; i < nums.Length; ++i)
+            array[i] = array[i - 1] >= array[i - 2] + nums[i] ? array[i - 1] : array[i - 2] + nums[i];
+        return array[nums.Length - 1];
+    }
+}
