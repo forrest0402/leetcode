54fcf653e37942e004bdf8488e4746b8f5b5f192
forrest <forrest0402@gmail.com>
2016-09-16 21:02:22 +0800

    38

---
+++ C#/27. Remove Element.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        List<int> array = new List<int>();
+        foreach (int i in nums)
+            if (i != val)
+                array.Add(i);
+        for (int i = 0; i < array.Count; ++i)
+            nums[i] = array[i];
+        return array.Count;
+    }
+}
