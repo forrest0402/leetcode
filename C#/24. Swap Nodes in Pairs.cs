b928deab4d8ec14cc7c50e78e04f1440ca8aabd9
forrest <forrest0402@gmail.com>
2016-09-16 12:57:03 +0800

    34-2

---
+++ C#/24. Swap Nodes in Pairs.cs
@@ -0,0 +1,37 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public ListNode SwapPairs(ListNode head)
+    {
+        int count = 0;
+        ListNode ans = head;
+        ListNode pre = null;
+        while (head != null && head.next != null)
+        {
+            if (count % 3 != 0)
+            {
+                head = head.next;
+                ++count;
+                continue;
+            }
+            ListNode temp = head;
+            head = head.next;
+            temp.next = head.next;
+            head.next = temp;
+            if (pre != null)
+                pre.next = head;
+            pre = temp;
+            if (count == 0)
+                ans = head;
+            ++count;
+        }
+        return ans;
+    }
+}
