226a5b849bbcb893588877a51c2a363e9732e4e3
forrest <forrest0402@gmail.com>
2016-09-16 21:02:00 +0800

    37

---
+++ C#/66. Plus One.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        if (digits == null)
+            return null;
+        if (digits.Length == 0)
+            return digits;
+        bool flag = false;
+        int carry = 10;
+        digits[digits.Length - 1]++;
+        for (int i = digits.Length - 1; i >= 0; --i)
+        {
+            if (flag)
+                digits[i]++;
+            if (digits[i] >= carry)
+            {
+                flag = true;
+                digits[i] -= carry;
+            }
+            else
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (!flag)
+            return digits;
+        else
+        {
+            List<int> tempList = digits.ToList();
+            tempList.Insert(0, 1);
+            return tempList.ToArray();
+        }
+    }
+}
