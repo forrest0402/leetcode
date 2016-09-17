08640f0419c9e0255182f19ac1184dceb22b0aaa
forrest <forrest0402@gmail.com>
2016-09-17 20:12:47 +0800

    50

---
+++ C#/9. Palindrome Number.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    int Pow(int n)
+    {
+        int sum = 1;
+        while (n-- > 0)
+            sum *= 10;
+        return sum;
+    }
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+        int length = 0, num = x, y = 0;
+        while (num > 0)
+        {
+            num /= 10;
+            ++length;
+        }
+        num = x;
+        while (num > 0)
+        {
+            y += num % 10 * Pow(--length);
+            num /= 10;
+        }
+        return y == x;
+    }
+}
