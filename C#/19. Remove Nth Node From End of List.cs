25065e98e2ac8ff6021f1188701a443f62e788db
forrest <forrest0402@gmail.com>
2016-09-19 16:59:38 +0800

    55

---
+++ C#/19. Remove Nth Node From End of List.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        int count = 0;
+        ListNode temp = head;
+        while (temp != null)
+        {
+            temp = temp.next;
+            ++count;
+        }
+        if (count == n)
+            return head.next;
+        temp = head;
+        for (int i = 1; i < count - n; ++i)
+            temp = temp.next;
+        temp.next = temp.next.next;
+        return head;
+    }
+}
