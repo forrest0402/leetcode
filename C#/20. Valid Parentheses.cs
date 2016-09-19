138376ad43dba97b1d0457a26ab88f7369640472
forrest <forrest0402@gmail.com>
2016-09-19 17:14:19 +0800

    56

---
+++ C#/20. Valid Parentheses.cs
@@ -0,0 +1,42 @@
+﻿public class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        foreach (char chs in s)
+        {
+            if (chs == '(' || chs == '{' || chs == '[')
+            {
+                stack.Push(chs);
+            }
+            if (chs == ')')
+            {
+                if (stack.Count == 0)
+                    return false;
+                char top = stack.Peek();
+                if (top == '(')
+                    stack.Pop();
+                else return false;
+            }
+            if (chs == '}')
+            {
+                if (stack.Count == 0)
+                    return false;
+                char top = stack.Peek();
+                if (top == '{')
+                    stack.Pop();
+                else return false;
+            }
+            if (chs == ']')
+            {
+                if (stack.Count == 0)
+                    return false;
+                char top = stack.Peek();
+                if (top == '[')
+                    stack.Pop();
+                else return false;
+            }
+        }
+        return stack.Count == 0;
+    }
+}
