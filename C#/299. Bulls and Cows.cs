910bc7ff2eb863b6ee7ca62a791bcdcf47de9751
forrest <forrest0402@gmail.com>
2016-09-19 11:31:33 +0800

    51-3

---
+++ C#/299. Bulls and Cows.cs
@@ -0,0 +1,39 @@
+﻿public class Solution
+{
+    public string GetHint(string secret, string guess)
+    {
+        bool[] charg = new bool[secret.Length];
+        bool[] chars = new bool[secret.Length];
+        int A = 0, B = 0;
+        for (int i = 0; i < guess.Length; ++i)
+        {
+            if (secret[i] == guess[i])
+            {
+                chars[i] = true;
+                charg[i] = true;
+                ++A;
+            }
+        }
+        for (int i = 0; i < guess.Length; ++i)
+        {
+            if (chars[i])
+                continue;
+            int index = -1;
+            for (int j = 0; j < secret.Length; ++j)
+            {
+                if (charg[j])
+                    continue;
+                if (secret[j] == guess[i])
+                    index = j;
+            }
+            if (index >= 0 && !charg[index])
+            {
+                charg[index] = true;
+                if (index == i)
+                    ++A;
+                else ++B;
+            }
+        }
+        return A + "A" + B + "B";
+    }
+}
