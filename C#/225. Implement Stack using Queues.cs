3bc529a102d69a5f097876ef29be9d7ba602fb58
forrest <forrest0402@gmail.com>
2016-09-19 19:56:04 +0800

    58

---
+++ C#/225. Implement Stack using Queues.cs
@@ -0,0 +1,38 @@
+﻿public class Stack
+{
+    // Push element x onto stack.
+    Queue<int> queue = new Queue<int>();
+    public void Push(int x)
+    {
+        Queue<int> temp = new Queue<int>();
+        while (queue.Count > 0)
+        {
+            temp.Enqueue(queue.Peek());
+            queue.Dequeue();
+        }
+        queue.Enqueue(x);
+        while (temp.Count > 0)
+        {
+            queue.Enqueue(temp.Peek());
+            temp.Dequeue();
+        }
+    }
+
+    // Removes the element on top of the stack.
+    public void Pop()
+    {
+        queue.Dequeue();
+    }
+
+    // Get the top element.
+    public int Top()
+    {
+        return queue.Peek();
+    }
+
+    // Return whether the stack is empty.
+    public bool Empty()
+    {
+        return queue.Count == 0;
+    }
+}
