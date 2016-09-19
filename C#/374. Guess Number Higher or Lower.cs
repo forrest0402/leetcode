46b19fe74e40279f923ce0c1ad5361e2359c8f86
forrest <forrest0402@gmail.com>
2016-09-19 14:14:30 +0800

    52-interesting

---
+++ C#/374. Guess Number Higher or Lower.cs
@@ -0,0 +1,23 @@
+﻿/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+// This is Java version
+public class Solution extends GuessGame
+{
+    public int guessNumber(int n) {
+        int low = 1, high = n, number = 1;
+        while (low <= high)
+        {
+            number = (low + high) >>> 1;
+            int ans = guess(number);
+            if (ans == 0)
+                return number;
+            if (ans == -1)
+                high = number - 1;
+            else low = number + 1;
+        }
+        return -1;
+    }
+}
