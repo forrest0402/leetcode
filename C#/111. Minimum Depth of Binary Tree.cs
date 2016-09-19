7594cf330e34b578eab26eb5d8ce1bf4f6639ea4
forrest <forrest0402@gmail.com>
2016-09-19 15:34:52 +0800

    53

---
+++ C#/111. Minimum Depth of Binary Tree.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    int GetDepth(TreeNode root)
+    {
+        if (root == null)
+            return 0;
+        if (root.left == null)
+            return GetDepth(root.right) + 1;
+        if (root.right == null)
+            return GetDepth(root.left) + 1;
+        return Math.Min(GetDepth(root.left), GetDepth(root.right)) + 1;
+    }
+    public int MinDepth(TreeNode root)
+    {
+        return GetDepth(root);
+    }
+}
