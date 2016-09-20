9899f21fcb5f8d7c997785a46dc7ea264e7be191
forrest <forrest0402@gmail.com>
2016-09-20 19:43:26 +0800

    68

---
+++ C#/7. Reverse Integer.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int Reverse(int x)
+    {
+        long n = x;
+        bool flag = x > 0;
+        n = Math.Abs(n);
+        char[] array = n.ToString().ToCharArray();
+        Array.Reverse(array);
+        long ans = long.Parse(new string(array));
+        if ((flag && ans > int.MaxValue) || (!flag && -ans < int.MinValue))
+            return 0;
+        return flag ? int.Parse(new string(array)) : -int.Parse(new string(array));
+    }
+}
