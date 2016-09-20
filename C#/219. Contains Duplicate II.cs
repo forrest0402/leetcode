2f3b80af16b3f06eed896f794351f7597e98c899
forrest <forrest0402@gmail.com>
2016-09-20 10:44:14 +0800

    62

---
+++ C#/219. Contains Duplicate II.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            if (dic.ContainsKey(nums[i]))
+            {
+                int pre = dic[nums[i]];
+                if (i - pre <= k)
+                    return true;
+                dic[nums[i]] = i;
+            }
+            else dic.Add(nums[i], i);
+        }
+        return false;
+    }
+}
