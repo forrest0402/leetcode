8815b277cc780f3fecd4782657b95b92338869dc
forrest <forrest0402@gmail.com>
2016-09-20 10:58:08 +0800

    63

---
+++ C#/58. Length of Last Word.cs
@@ -0,0 +1,10 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        if (s == "")
+            return 0;
+        string[] array = s.Trim(' ').Split(' ');
+        return array[array.Length - 1].Length;
+    }
+}
