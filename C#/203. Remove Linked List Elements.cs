2774bf3322c96b50afe2f28b77774e5aac709a0a
forrest <forrest0402@gmail.com>
2016-09-20 20:07:48 +0800

    70

---
+++ C#/203. Remove Linked List Elements.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        if (head == null)
+            return null;
+        ListNode ans = head;
+        while (ans.next != null)
+        {
+            if (ans.next.val == val)
+                ans.next = ans.next.next;
+            else ans = ans.next;
+        }
+        if (head.val == val)
+            head = head.next;
+        return head;
+    }
+}
