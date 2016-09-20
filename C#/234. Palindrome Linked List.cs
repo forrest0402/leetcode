49948a5f7a25f1eb18fe1cab9aa45e689b0fc7bd
forrest <forrest0402@gmail.com>
2016-09-20 15:08:37 +0800

    65

---
+++ C#/234. Palindrome Linked List.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+ //not a proper solution
+public class Solution
+{
+    public bool IsPalindrome(ListNode head)
+    {
+        if (head == null)
+            return true;
+        Stack<int> stack = new Stack<int>();
+        ListNode t = head;
+        while (t != null)
+        {
+            stack.Push(t.val);
+            t = t.next;
+        }
+        t = head;
+        while (t != null)
+        {
+            if (t.val != stack.Peek())
+                return false;
+            stack.Pop();
+            t = t.next;
+        }
+        return true;
+    }
+}
