52e3aaf7a15a61f18877328e0fd49219adc20990
forrest <forrest0402@gmail.com>
2016-09-20 19:38:11 +0800

    67

---
+++ C#/190. Reverse Bits.cs
@@ -0,0 +1,9 @@
+﻿public class Solution
+{
+    public uint reverseBits(uint n)
+    {
+        char[] arrays = Convert.ToString(n, 2).PadLeft(32, '0').ToCharArray();
+        Array.Reverse(arrays);
+        return Convert.ToUInt32(new string(arrays), 2);
+    }
+}
