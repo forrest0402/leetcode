f1106964b47c8a45b376f0e63ec9ca9e0f24cf53
forrest <forrest0402@gmail.com>
2016-09-21 13:45:56 +0800

    72-2

---
+++ C#/396. Rotate Function.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int MaxRotateFunction(int[] A)
+    {
+        if (A.Length == 0)
+            return 0;
+        int pre = 0, num = 0;
+        for (int i = 0; i < A.Length; ++i)
+        {
+            pre += A[i] * i;
+            num += A[i];
+        }
+        int maxValue = pre;
+        for (int i = 1; i < A.Length; ++i)
+        {
+            pre += num - A.Length * A[A.Length - i];
+            if (pre > maxValue)
+                maxValue = pre;
+        }
+        return maxValue;
+    }
+}
