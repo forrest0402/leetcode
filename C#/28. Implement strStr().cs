2d562e6d0d7d4cb83d757577638abba1343961b6
forrest <forrest0402@gmail.com>
2016-09-21 17:00:20 +0800

    75

---
+++ C#/28. Implement strStr().cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        for (int i = 0; i <= haystack.Length - needle.Length; ++i)
+        {
+            bool flag = true;
+            for (int j = 0; j < needle.Length; ++j)
+            {
+                if (haystack[i + j] != needle[j])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+                return i;
+        }
+        return -1;
+    }
+}
