8d229c7cfd67108ca5d8f59db7296f60092dfb79
forrest <forrest0402@gmail.com>
2016-09-21 15:51:12 +0800

    73

---
+++ C#/1. Two Sum.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        if (nums == null || nums.Length < 2)
+            return new int[2];
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            for (int j = nums.Length - 1; j > i; --j)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return ans;
+    }
+}
