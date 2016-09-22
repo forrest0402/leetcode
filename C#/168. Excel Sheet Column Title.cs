dd2c9611798177063674c5a4672bb2392e897c10
forrest <forrest0402@gmail.com>
2016-09-22 22:56:47 +0800

    80-2

---
+++ C#/168. Excel Sheet Column Title.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public string ConvertToTitle(int n)
+    {
+        string ans = "";
+        if (n == 1)
+            return "A";
+        while (n > 0)
+        {
+            int remainder = n % 26;
+            n /= 26;
+            if (remainder == 0)
+                --n;
+            ans = remainder == 0 ? 'Z' + ans : (char)(remainder - 1 + 'A') + ans;
+        }
+        return ans;
+    }
+}
