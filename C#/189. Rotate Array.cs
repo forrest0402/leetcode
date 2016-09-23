1d82742623fc052a8a2155077796c7c900272148
forrest <forrest0402@gmail.com>
2016-09-23 11:20:48 +0800

    82-2

---
+++ C#/189. Rotate Array.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public void Rotate(int[] nums, int k)
+    {
+        if (nums == null || nums.Length == 0 || k == 0)
+            return;
+        int n1 = nums[0], curN = n1, pos = 0, count = 0;
+        bool first = true;
+        HashSet<int> visited = new HashSet<int>();
+        while (count < nums.Length)
+        {
+            while (visited.Contains(pos))
+            {
+                pos = (pos + 1) % nums.Length;
+                curN = nums[pos];
+            }
+            visited.Add(pos);
+            int rPos = (pos + k) % nums.Length;
+            int temp = nums[rPos];
+            nums[rPos] = curN;
+            curN = temp;
+            pos = rPos;
+            ++count;
+        }
+    }
+}
