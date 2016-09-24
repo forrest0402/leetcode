53560d94a04f7c0b206533c82db9ce7bf02a9074
forrest <forrest0402@gmail.com>
2016-09-24 11:33:28 +0800

    86

---
+++ C#/167. Two Sum II - Input array is sorted.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        int index1 = 0, index2 = 0;
+        for (int i = 0; i < numbers.Length - 1; ++i)
+        {
+            index1 = i;
+            int l = index1 + 1, r = numbers.Length - 1, mid = 0, n = target - numbers[index1];
+            while (l <= r)
+            {
+                mid = l + (r - l) / 2;
+                if (numbers[mid] == n)
+                    break;
+                if (numbers[mid] > n)
+                    r = mid - 1;
+                else l = mid + 1;
+            }
+            if (numbers[mid] == n)
+            {
+                index2 = mid;
+                break;
+            }
+        }
+        return new int[] { index1 + 1, index2 + 1 };
+    }
+}
