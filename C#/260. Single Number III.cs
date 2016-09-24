922c37def3faa6be83b5a03b15e45472eaa93f3f
forrest <forrest0402@gmail.com>
2016-09-24 11:41:37 +0800

    87

---
+++ C#/260. Single Number III.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int[] SingleNumber(int[] nums)
+    {
+        if (nums == null || nums.Length < 2)
+            return new int[] { 0, 0 };
+        HashSet<int> set = new HashSet<int>();
+        foreach (int i in nums)
+        {
+            if (!set.Contains(i))
+                set.Add(i);
+            else set.Remove(i);
+        }
+        return set.ToArray();
+    }
+}
