c78757ae597dc901e760992a99cc50de11cb8992
forrest <forrest0402@gmail.com>
2016-09-27 18:59:24 +0800

    90-2-interesting

---
+++ C#/122. Best Time to Buy and Sell Stock II.cs
@@ -0,0 +1,23 @@
+﻿class Solution
+{
+    public:
+    int maxProfit(vector<int>& prices)
+    {
+        int n = prices.size();
+        if (n <= 1) return 0;
+        vector<int> dp(n,0);
+        int minPrice = prices[0], res = 0;
+        for (int i = 1; i < n; ++i)
+        {
+            minPrice = min(minPrice, prices[i]);
+            if (prices[i] - minPrice >= dp[i - 1]) dp[i] = prices[i] - minPrice;
+            else
+            {
+                minPrice = prices[i];
+                res += dp[i - 1];
+            }
+        }
+
+        return res + dp[n - 1];
+    }
+};
