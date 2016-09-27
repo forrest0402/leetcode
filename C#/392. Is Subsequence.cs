98481ef094369c1bba927d475ea72e89134621c0
forrest <forrest0402@gmail.com>
2016-09-27 19:06:52 +0800

    91

---
+++ C#/392. Is Subsequence.cs
@@ -0,0 +1,45 @@
+﻿public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s == "")
+            return true;
+        bool flag = false;
+        int index = 0, find = 0;
+        foreach (char chs in s)
+        {
+            bool hasFind = false;
+            while (index < t.Length)
+            {
+                if (t[index++] == chs)
+                {
+                    hasFind = true;
+                    break;
+                }
+            }
+            if (hasFind)
+                ++find;
+            else break;
+            if (find == s.Length)
+                flag = true;
+        }
+        return flag;
+    }
+}
+
+//
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int prev = 0;
+        foreach (char chs in s)
+        {
+            prev = t.IndexOf(chs, prev);
+            if (prev == -1)
+                return false;
+            ++prev;
+        }
+        return true;
+    }
+}
