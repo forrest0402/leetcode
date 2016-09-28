e1560ef067ee29bb321dc0d1926d17340a7155d0
forrest <forrest0402@gmail.com>
2016-09-28 15:28:21 +0800

    93

--- C#/343. Integer Break.cs
+++ C#/343. Integer Break.cs
@@ -1,39 +1,14 @@
-﻿public class Solution
-{
-    public int MissingNumber(int[] nums)
-    {
-        int sum = 0, maxN = 0;
-        foreach (int i in nums)
-        {
-            if (i > maxN)
-                maxN = i;
-            sum ^= i;
+﻿public class Solution {
+    public int IntegerBreak(int n) {
+        if(n == 1 || n == 2)
+            return 1;
+        if(n == 3)
+            return 2;
+        int count = 1;
+        while(n > 4){
+            n -= 3;
+            count *= 3;
         }
-        if (maxN + 1 == nums.Length)
-            return maxN + 1;
-        for (int i = 0; i <= nums.Length; ++i)
-            sum ^= i;
-        return sum;
-    }
-}
-//
-public class Solution
-{
-    public int MissingNumber(int[] nums)
-    {
-        int sum = 0, ans = 0, maxN = 0;
-        for (int i = 0; i <= nums.Length; ++i)
-        {
-            if (i < nums.Length)
-            {
-                sum ^= nums[i];
-                if (nums[i] > maxN)
-                    maxN = nums[i];
-            }
-            ans ^= i;
-        }
-        if (maxN + 1 == nums.Length)
-            return maxN + 1;
-        return sum ^ ans;
+        return count * n;
     }
 }
