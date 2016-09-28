3adcd0484a21ba4424018ab03c957fce647191c3
forrest <forrest0402@gmail.com>
2016-09-28 12:37:39 +0800

    93

---
+++ C#/343. Integer Break.cs
@@ -0,0 +1,39 @@
+﻿public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int sum = 0, maxN = 0;
+        foreach (int i in nums)
+        {
+            if (i > maxN)
+                maxN = i;
+            sum ^= i;
+        }
+        if (maxN + 1 == nums.Length)
+            return maxN + 1;
+        for (int i = 0; i <= nums.Length; ++i)
+            sum ^= i;
+        return sum;
+    }
+}
+//
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
+        int sum = 0, ans = 0, maxN = 0;
+        for (int i = 0; i <= nums.Length; ++i)
+        {
+            if (i < nums.Length)
+            {
+                sum ^= nums[i];
+                if (nums[i] > maxN)
+                    maxN = nums[i];
+            }
+            ans ^= i;
+        }
+        if (maxN + 1 == nums.Length)
+            return maxN + 1;
+        return sum ^ ans;
+    }
+}
