9d58581e3e1d1f703869648b233f7e04ed7e0743
forrest <forrest0402@gmail.com>
2016-09-28 11:29:33 +0800

    92

---
+++ C#/357. Count Numbers with Unique Digits.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int CountNumbersWithUniqueDigits(int n)
+    {
+        if (n == 0)
+            return 1;
+        int count = 10;
+        for (int i = 1; i < n; ++i)
+        {
+            int num = 9;
+            for (int j = 1; j <= i; ++j)
+                num *= (10 - j);
+            if (num < 0)
+                num = 0;
+            count += num;
+        }
+        return count;
+    }
+}
