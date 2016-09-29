1c476a973feb175c51178804c12e0e0d0d40fa1a
forrest <forrest0402@gmail.com>
2016-09-29 11:25:30 +0800

    96

---
+++ C#/144. Binary Tree Preorder Traversal.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    private void Tranverse(TreeNode node, IList<int> list)
+    {
+        if (node == null)
+            return;
+        list.Add(node.val);
+        Tranverse(node.left, list);
+        Tranverse(node.right, list);
+    }
+    public IList<int> PreorderTraversal(TreeNode root)
+    {
+        IList<int> list = new List<int>();
+        Tranverse(root, list);
+        return list;
+    }
+}
