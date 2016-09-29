e74a35dc642b7cca6ccf491057a3f9c2c8e470ac
forrest <forrest0402@gmail.com>
2016-09-29 11:21:40 +0800

    95

---
+++ C#/12. Integer to Roman.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public string IntToRoman(int num) {
+        string[] symbol = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        int[] value = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        int index = 0;
+        string ans = "";
+        while(num != 0){
+            if(value[index] <= num){
+                num -= value[index];
+                ans += symbol[index];
+            }
+            else ++index;
+        }
+        return ans;
+    }
+}
