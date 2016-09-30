3348789e85b935bed821fda71a93755e9f2e3165
forrest <forrest0402@gmail.com>
2016-09-30 15:33:00 +0800

    99-2-interesting

---
+++ C#/319. Bulb Switcher.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int BulbSwitch(int n)
+    {
+        int count = 0, num = 0;
+        while (num * num < n)
+        {
+            ++count;
+            ++num;
+        }
+        if (num * num == n)
+            return count;
+        return count - 1;
+    }
+}
