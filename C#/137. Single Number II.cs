b29111153493dc0832fc8746afc8b24b3a5fbca1
forrest <forrest0402@gmail.com>
2016-10-10 11:10:02 +0800

    very interesting

---
+++ C#/137. Single Number II.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int[] bit = new int[32];
+        int ans = 0;
+        for (int b = 0; b < 32; ++b)
+        {
+            foreach (int i in nums)
+                bit[b] += (i >> b) & 1;
+            ans |= (bit[b] % 3) << b;
+        }
+        return ans;
+    }
+}
