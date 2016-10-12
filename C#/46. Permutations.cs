d1586b10ef1d36f8c04ab5114bdfce43f1870faa
forrest <forrest0402@gmail.com>
2016-10-12 19:06:01 +0800

    112

---
+++ C#/46. Permutations.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    private List<IList<int>> DFS(List<int> list)
+    {
+        List<IList<int>> ans = new List<IList<int>>();
+        if (list.Count == 1)
+        {
+            ans.Add(new List<int>() { list[0] });
+            return ans;
+        }
+        int n = list[0];
+        list.RemoveAt(0);
+        List<IList<int>> allPer = DFS(list);
+        foreach (IList<int> p in allPer)
+        {
+            for (int i = 0; i <= p.Count; ++i)
+            {
+                List<int> newP = new List<int>(p);
+                newP.Insert(i, n);
+                ans.Add(newP);
+            }
+        }
+        return ans;
+    }
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        return DFS(nums.ToList());
+    }
+}
