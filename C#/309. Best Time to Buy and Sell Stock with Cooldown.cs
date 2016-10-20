a69a42d917f36b5f6e73e67d4483e284a16aea52
forrest <forrest0402@gmail.com>
2016-10-20 11:08:42 +0800

    113 - very interesting

---
+++ C#/309. Best Time to Buy and Sell Stock with Cooldown.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int[] sell = new int[prices.Length], nosell = new int[prices.Length], dp = new int[prices.Length];
+        dp[0] = sell[0] = prices[0];
+        int minBuy1 = prices[0], minBuy2 = 0;
+        for (int i = 1; i < prices.Length; ++i)
+        {
+            dp[i - 1] = Math.Max(sell[i - 1], nosell[i - 1]);
+            sell[i] = prices[i] > minBuy1 ? nosell[i - 1] + prices[i] - minBuy1 : nosell[i - 1];
+            if (sell[i - 1] > nosell[i - 1])
+            {
+                nosell[i] = sell[i - 1];
+                minBuy1 = minBuy2;
+            }
+            else
+                nosell[i] = nosell[i - 1];
+            if (minBuy1 > prices[i])
+                minBuy1 = prices[i];
+        }
+        return Math.Max(sell[prices.Length - 1], nosell[prices.Length - 1]);
+    }
+}
