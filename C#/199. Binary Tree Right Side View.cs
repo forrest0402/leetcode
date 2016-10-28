852f476261623c4f679645c73e38f561a1863d4f
forrest0402 <forrest0402@hotmail.com>
2016-10-28 14:28:25 +0800

    121

---
+++ C#/199. Binary Tree Right Side View.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution
+{
+    IList<int> TraByLevelOrder = null;
+    public void Traverse(TreeNode node, int level)
+    {
+        if (node == null)
+            return;
+        if (TraByLevelOrder.Count - 1 >= level)
+        {
+            if (node.right != null)
+                TraByLevelOrder[level] = node.right.val;
+            else if (node.left != null)
+                TraByLevelOrder[level] = node.left.val;
+        }
+        else
+        {
+            if (node.right != null)
+                TraByLevelOrder.Add(node.right.val);
+            else if (node.left != null)
+                TraByLevelOrder.Add(node.left.val);
+        }
+        Traverse(node.left, level + 1);
+        Traverse(node.right, level + 1);
+    }
+    public IList<int> RightSideView(TreeNode root)
+    {
+        if (root == null)
+            return new List<int>();
+        TraByLevelOrder = new List<int>() { root.val };
+        Traverse(root, 1);
+        return TraByLevelOrder;
+    }
+}
