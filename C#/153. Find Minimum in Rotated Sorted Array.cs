fd08ea092e9cf240683418111e1ace60c029e2b8
forrest0402 <forrest0402@hotmail.com>
2016-10-28 14:26:58 +0800

    119

---
+++ C#/153. Find Minimum in Rotated Sorted Array.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int FindMin(int[] nums)
+    {
+        int l = 0, r = nums.Length - 1, mid = 0, ans = nums[0];
+        while (l <= r)
+        {
+            if (l == r)
+            {
+                ans = nums[l];
+                break;
+            }
+            if (nums[l] < nums[r])
+            {
+                ans = nums[l];
+                break;
+            }
+            mid = l + (r - l) / 2;
+            if (nums[l] <= nums[mid])
+                ++l;
+            else r = mid;
+        }
+        return ans;
+    }
+}
+
+
+public class Solution
+{
+    public int FindMin(int[] nums)
+    {
+        Array.Sort(nums);
+        return nums[0];
+    }
+}
