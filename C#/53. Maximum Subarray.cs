9f84f3d639d36a595d3c4777620758136af3bfc8
forrest0402 <forrest0402@hotmail.com>
2016-10-28 14:25:56 +0800

    118

---
+++ C#/53. Maximum Subarray.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int sum = 0, maxsum = nums[0];
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            sum = Math.Max(sum + nums[i], nums[i]);
+            if (sum > maxsum)
+                maxsum = sum;
+        }
+        return maxsum;
+    }
+}
