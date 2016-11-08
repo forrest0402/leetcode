0ca32ddd47b3c9f36468f5dd1b413a6d81d31532
forrest0402 <forrest0402@hotmail.com>
2016-11-08 22:12:22 +0800

    125-interesting

---
+++ C#/240. Search a 2D Matrix II.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public bool SearchMatrix(int[,] matrix, int target)
+    {
+        int row = 0, col = matrix.GetLength(1) - 1, len = matrix.GetLength(0);
+        while (row < len && col >= 0)
+        {
+            if (matrix[row, col] == target) return true;
+            if (matrix[row, col] > target) --col;
+            else ++row;
+        }
+        return false;
+    }
+}
