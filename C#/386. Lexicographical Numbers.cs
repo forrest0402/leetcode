db86552883163303ab5228200a6ac9b02abc3bc7
forrest0402 <forrest0402@hotmail.com>
2016-11-08 22:11:33 +0800

    124

---
+++ C#/386. Lexicographical Numbers.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    List<int> ans = new List<int>();
+    void AddNum(int num, int n, int addN)
+    {
+        if (num + addN <= n)
+            ans.Add(num + addN);
+        else return;
+        AddNum((num + addN) * 10, n, 0);
+        if ((num == 1 && addN <= 7) || (num > 1 && addN <= 8))
+            AddNum(num, n, addN + 1);
+    }
+    public IList<int> LexicalOrder(int n)
+    {
+        AddNum(1, n, 0);
+        return ans;
+    }
+}
