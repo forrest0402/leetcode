27f38c7b7dd73055c1250aa6ea7808564792fa0f
forrest0402 <forrest0402@hotmail.com>
2020-07-20 00:55:24 +0800

    1019

--- python/1019. Next Greater Node In Linked List.py
+++ python/1019. Next Greater Node In Linked List.py
@@ -14,7 +14,33 @@ class ListNode:
 
 
 class Solution:
+
     def nextLargerNodes(self, head: ListNode) -> List[int]:
+        ret = []
+        un_match = []
+        found = False
+        last_n = 10 ** 9
+        while head:
+            idx = len(ret)
+            ret.append(0)
+            if not (head.val <= last_n and not found):
+                found = False
+                while len(un_match) > 0:
+                    (v, i) = un_match[-1]
+                    if head.val > v:
+                        ret[i] = head.val
+                        found = True
+                        un_match.pop()
+                    else:
+                        break
+
+            un_match.append((head.val, idx))
+            last_n = head.val
+            head = head.next
+
+        return ret
+
+    def nextLargerNodes2(self, head: ListNode) -> List[int]:
         ret = []
         un_match = dict()
         found = False
