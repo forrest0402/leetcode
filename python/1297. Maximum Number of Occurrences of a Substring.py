55a02d593db45bc17dae03e3ed72f2d2aec0f4bb
forrest0402 <forrest0402@hotmail.com>
2020-08-12 12:44:33 +0800

    1297

--- python/1297. Maximum Number of Occurrences of a Substring.py
+++ python/1297. Maximum Number of Occurrences of a Substring.py
@@ -9,7 +9,26 @@ from collections import defaultdict
 
 
 class Solution:
+
     def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
+        ret = 0
+        n = minSize
+        bucket = defaultdict(int)
+        pi = 0
+        for i in range(n, len(s) + 1):
+            sub = s[pi:i]
+            pi += 1
+            if len(set(sub)) > maxLetters:
+                continue
+            bucket[sub] += 1
+
+        # print(bucket)
+        if len(bucket) > 0:
+            ret = max(ret, max([v for k, v in bucket.items()]))
+
+        return ret
+
+    def maxFreq2(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
         ret = 0
         for n in range(minSize, maxSize + 1):
             bucket = defaultdict(int)
